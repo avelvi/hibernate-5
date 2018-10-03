1fb22ca06024b179a1f21ab3797b12a709a6a7de
Aleksey Ivlev <aleksey.velvi@gmail.com>
2018-10-03 22:13:55 +0300

    Chapter 02

---
+++ chapter-02/src/main/java/com/avelvi/hibernate/chapter02/entities/Message.java
@@ -0,0 +1,67 @@
+package com.avelvi.hibernate.chapter02.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Message {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false)
+    private String text;
+
+    public Message() {
+    }
+
+    public Message(String text) {
+        setText(text);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Message message = (Message) o;
+
+        if (!id.equals(message.id)) return false;
+        return text.equals(message.text);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id.hashCode();
+        result = 31 * result + text.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuffer sb = new StringBuffer("Message{");
+        sb.append("id=").append(id);
+        sb.append(", text='").append(text).append('\'');
+        sb.append('}');
+        return sb.toString();
+    }
+}
